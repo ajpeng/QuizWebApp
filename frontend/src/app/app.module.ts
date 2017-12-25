b8e4f14402139f85c115821ea9d19139049a612c
Alex Peng <jjpeng@ryerson.ca>
2017-12-25 10:45:05 -0500

    added nav component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,23 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule , MatInputModule, MatCardModule , MatListModule} from '@angular/material';
+import {MatButtonModule , MatInputModule, MatCardModule , MatListModule, MatToolbarModule} from '@angular/material';
 import {FormsModule} from '@angular/forms'
-
-
+import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component'
 import { ApiService } from './api.service'
 import { QuestionsComponent} from './questions.component'
+import { HomeComponent } from './home.component'
+import { NavComponent } from './nav.component'
+
+const routes = [
+  { path : '' , component: HomeComponent},
+  { path : 'question' , component: QuestionComponent},
+  { path : 'questions' , component: QuestionsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent , QuestionComponent, QuestionsComponent
+    AppComponent , QuestionComponent, QuestionsComponent, HomeComponent, NavComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule,
-    MatButtonModule, MatInputModule, MatCardModule , MatListModule,
-    FormsModule , HttpClientModule, 
+    
+    BrowserModule, BrowserAnimationsModule,RouterModule.forRoot(routes),
+    MatButtonModule, MatInputModule, MatCardModule , MatListModule, MatToolbarModule,
+    FormsModule , HttpClientModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav',
+  template: `
+  <mat-toolbar>My App</mat-toolbar>
+  `
+
+})
+export class NavComponent {
+}
+
+
+    // <button mat-button routerLink="/">Quiz</button>
+        // <button mat-button routerLink="/question">Question</button>
+        // <button mat-button routerLink="/questions">Question List</button>
