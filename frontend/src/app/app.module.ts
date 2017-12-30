9bed4cafa8463dcd7722e441471d72460eaa893c
Alex Peng <jjpeng@ryerson.ca>
2017-12-29 20:58:49 -0500

    added autherization header with a bearer token

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule , MatInputModule, MatCardModule , MatListModule, MatToolbarModule} from '@angular/material';
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component'
@@ -13,12 +13,16 @@ import { HomeComponent } from './home.component'
 import { NavComponent } from './nav.component'
 import { QuizComponent } from './quiz.component'
 import { QuizzesComponent } from './quizzes.component'
+import { RegisterComponent} from './register.component'
+import { AuthService} from './auth.service'
+import { AuthIntercepter} from './auth.interceptor'
+
 
 const routes = [
   { path : '' , component: HomeComponent},
   { path : 'question' , component: QuestionComponent},
   { path : 'question/:quizId' , component: QuestionComponent},
-  { path : 'questions' , component: QuestionsComponent},
+  { path : 'register' , component: RegisterComponent},
   { path : 'quiz' , component: QuizComponent},
   { path : 'quizzes' , component: QuizzesComponent}
 ]
@@ -26,15 +30,19 @@ const routes = [
 @NgModule({
   declarations: [
     AppComponent , QuestionComponent, QuestionsComponent, HomeComponent, NavComponent, QuizComponent,
-    QuizzesComponent
+    QuizzesComponent , RegisterComponent
   ],
   imports: [
     
     BrowserModule, BrowserAnimationsModule,RouterModule.forRoot(routes),
     MatButtonModule, MatInputModule, MatCardModule , MatListModule, MatToolbarModule,
-    FormsModule , HttpClientModule
+    FormsModule ,ReactiveFormsModule,  HttpClientModule
   ],
-  providers: [ApiService],
+  providers: [ApiService,AuthService,{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthIntercepter,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
